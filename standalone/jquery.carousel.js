5941c10eeecbd15364adb0117829099929c3f630
alvinim <alvinyim@octopusbusinessenterprise.com>
2013-04-27 02:24:28 +0800

    Correct carousel js version

--- standalone/jquery.carousel.js
+++ standalone/jquery.carousel.js
@@ -1,143 +1,159 @@
-(function( $ ){
+/*
+ * jQuery Carousel Plugin v1.0
+ * http://richardscarrott.co.uk/posts/view/jquery-carousel-plugin
+ *
+ * Copyright (c) 2010 Richard Scarrott
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Requires jQuery v1.4+
+ *
+ */
 
-    var settings = {};
-    var currentPage = 1;
-    var totalPages = 1;
+// prototypal inheritance
+if (typeof Object.create !== 'function') {
+	Object.create = function (o) {
+		function F() {}
+		F.prototype = o;
+		return new F();
+	};
+}
 
-    var methods = {
-        init : function( options ) {
-            settings = $.extend( {
-                'perpage' : 5
-                ,'width' : '800'
-				,'showPager': true
-				,'animationSpeed': 1000
-				,'animationType': 'swing'
-				,'startPage': 1
-				,'onPageChangeStart': null
-				,'onPageChangeEnd': null
-            }, options);
+(function($) {
+	// ie alias
+	var headache = $.browser.msie && $.browser.version.substr(0,1)<9;
 
-            var self = this;
+	// carousel
+	var Carousel = {
+		settings: {
+			itemsPerPage: 1,
+			itemsPerTransition: 1,
+			noOfRows: 1,
+			pagination: true,
+			nextPrevLinks: true,
+			speed: 'normal',
+			easing: 'swing'
+		},
+		init: function(el, options) {
+			if (!el.length) {return false;}
+			this.options = $.extend({}, this.settings, options);
+			this.itemIndex = 0;	
+			this.container = el;
+			this.runner = this.container.find('ul');
+			this.items = this.runner.children('li');
+			this.noOfItems = this.items.length;
+			this.setRunnerWidth();
+			if (this.noOfItems <= this.options.itemsPerPage) {return false;} // bail if there are too few items to paginate
+			this.insertMask();
+			this.noOfPages = Math.ceil((this.noOfItems - this.options.itemsPerPage) / this.options.itemsPerTransition) + 1;
+			if (this.options.pagination) {this.insertPagination();}
+			if (this.options.nextPrevLinks) {this.insertNextPrevLinks();}
+			this.updateBtnStyles();
+		},
+		insertMask: function() {
+			this.runner.wrap('<div class="mask" />');
+			this.mask = this.container.find('div.mask');
 
-            // paging
-            return this.each(function() {
-                var $this = $(this);
+			// set mask height so items can be of varying height
+			var maskHeight = this.runner.outerHeight(true);
+			this.mask = this.container.find('div.mask');
+			this.mask.height(maskHeight);
+		},
+		setRunnerWidth: function() {
+			this.noOfItems = Math.round(this.noOfItems / this.options.noOfRows);
+			var width =  this.items.outerWidth(true) * this.noOfItems;
+			this.runner.width(width);
+		},
+		insertPagination: function() {
+			var i, links = [];
+			this.paginationLinks = $('<ol class="pagination-links" />');
+			for (i = 0; i < this.noOfPages; i++) {
+				links[i] = '<li><a href="#item-' + i + '">' + (i + 1) + '</a></li>';
+			}
+			this.paginationLinks
+				.append(links.join(''))
+				.appendTo(this.container)
+				.find('a')
+					.bind('click.carousel', $.proxy(this, 'paginationHandler'));
+		},
+		paginationHandler: function(e) {
+			this.itemIndex = e.target.hash.substr(1).split('-')[1] * this.options.itemsPerTransition;
+			this.animate();
+			return false;
+		},
+		insertNextPrevLinks: function() {
+			this.prevLink = $('<a href="#" class="prev">Prev</a>')
+								.bind('click.carousel', $.proxy(this, 'prevItem'))
+								.appendTo(this.container);
+			this.nextLink = $('<a href="#" class="next">Next</a>')
+								.bind('click.carousel', $.proxy(this, 'nextItem'))
+								.appendTo(this.container);
+		},
+		nextItem: function() {
+			this.itemIndex = this.itemIndex + this.options.itemsPerTransition;
+			this.animate();
+			return false;
+		},
+		prevItem: function() {
+			this.itemIndex = this.itemIndex - this.options.itemsPerTransition;
+			this.animate();
+			return false;
+		},
+		updateBtnStyles: function() {
+			if (this.options.pagination) {
+				this.paginationLinks
+					.children('li')
+						.removeClass('current')
+						.eq(Math.ceil(this.itemIndex / this.options.itemsPerTransition))
+							.addClass('current');
+			}
 
-				// handle building of the pager
-				var ul = $(".carousel-holder ul", $this);
-				var lis = $("li", ul);
-				var liw = $(lis[0]).outerWidth();
-				ul.width(liw*lis.length); // set the correct width whilst we're here
-				var ulw = ul.outerWidth();
-				totalPages = Math.round(ulw/liw/settings.perpage);
-					
-				if(settings.showPager) {
-					for(var i = 0; i < totalPages; i++)
-					{
-						$(".controls", $this).append("<li><a href=\"#\"></a></li>");
-					}
-					$(".controls li:first-child", $this).addClass("selected");
-
-					// set up the events for the pager
-					$(".controls li a", $this).click(function () {
-
-						if(!$(".carousel-holder ul", $this).is(":animated"))
-						{
-							// adjust the margin
-							var index = $(this).parent().index();
-							var newPage = index+1;
-							$this.carousel("changePage", newPage);
-							currentPage = newPage;
-						}
-
-						return false;
-					});
+			if (this.options.nextPrevLinks) {
+				this.nextLink
+					.add(this.prevLink)
+						.removeClass('disabled');
+				if (this.itemIndex === (this.noOfItems - this.options.itemsPerPage)) {
+					this.nextLink.addClass('disabled');
+				} 
+				else if (this.itemIndex === 0) {
+					this.prevLink.addClass('disabled');
 				}
+			}
+		},
+		animate: function() {
+			var nextItem, pos;
+			// check whether there are enough items to animate to
+			if (this.itemIndex > (this.noOfItems - this.options.itemsPerPage)) {
+				this.itemIndex = this.noOfItems - this.options.itemsPerPage; // go to last panel - items per transition
+			}
+			if (this.itemIndex < 0) {
+				this.itemIndex = 0; // go to first
+			}
+			nextItem = this.items.eq(this.itemIndex);
+			pos = nextItem.position();
+			
+			if (headache) {
+				this.runner
+					.stop()
+					.animate({left: -pos.left}, this.options.speed, this.options.easing);
+			}
+			else {
+				this.mask
+					.stop()
+					.animate({scrollLeft: pos.left}, this.options.speed, this.options.easing);
+			}
+			this.updateBtnStyles();
+		}
+	};
 
-                // handle left/right arrows
-                $(".carousel-holder .arrowl", $this).click(function () {
-                    var carousel = $("ul", $(this).parent());
-                    if(!carousel.is(":animated")) {
-                        if(currentPage > 1) {
-                            var newPage = currentPage-1;
-                            $this.carousel("changePage", currentPage-1);
-                            currentPage = newPage;
-                        }
-                    }
-                    return false;
-                });
-                $(".carousel-holder .arrowr", $this).click(function () {
-                    var carousel = $("ul", $(this).parent());
-                    if(!carousel.is(":animated")) {
-                        if(currentPage < totalPages) {
-                            var newPage = currentPage+1;
-                            $this.carousel("changePage", newPage);
-                            currentPage = newPage;
-                        }
-                    }
-                    return false;
-                });
-
-                // hide all but the first item!
-                $(".items > div", $this).hide();
-                $(".items div:first-child", $this).show();
-
-                $(".carousel-holder li", $this).click(function () {
-                    var index = $(this).index()+1;
-
-                    $("li", $(this).parent()).removeClass("selected");
-                    $(this).addClass("selected");
-
-                    $(".items div.item", $this).hide();
-                    $(".items div.item[rel='" + index + "']", $this).show();
-
-                    return false;
-                });
-				
-				if(settings.startPage > 1 && settings.startPage <= totalPages) {
-					$this.carousel("changePage", settings.startPage, false);
-				}
-
-            });
-        },
-        changePage: function (page, animate) {
-            this.each(function () {
-
-				var ml = ((page-1)*-1*settings.width) + 'px';
-				
-                // animate the slide if we want to!
-				if(animate != false) {
-					if(jQuery.isFunction(settings.onPageChangeStart)) {
-						settings.onPageChangeStart.call(this, page);
-					}
-					$(".carousel-holder ul", $(this)).animate({'margin-left': ml}, settings.animationSpeed, settings.animationType, function () {
-						if(jQuery.isFunction(settings.onPageChangeEnd)) {
-							settings.onPageChangeEnd.call(this, page);
-						}
-					});
-				} else {
-					$(".carousel-holder ul").css({'margin-left': ml});
-				}
-
-                // update the paging icon!
-                $(".controls li.selected", $(this)).removeClass("selected");
-                $(".controls li:nth-child(" + (page) + ")", $(this)).addClass("selected");
-            });
-
-        }
-    };
-
-    $.fn.carousel = function( method ) {
-
-        // Method calling logic
-        if ( methods[method] ) {
-            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof method === 'object' || ! method ) {
-            return methods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.dcSlider' );
-        }
-
-    };
-
-})( jQuery );
+	// bridge
+	$.fn.carousel = function(options) {
+		return this.each(function() {
+			var obj = Object.create(Carousel);
+			obj.init($(this), options);
+			$.data(this, 'carousel', obj);
+		});
+	};
+})(jQuery);
